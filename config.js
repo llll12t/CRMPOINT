2a504120b680fdebc0309dfeba544c1f9deb8f41
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-07 15:18:28 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwzjlrbM1XmUhpUg1nWCij7-0wq1CRhixz-uA9cwv58qMK5Hp1xq03XZDeVnAR6C3WheA/exec',
+  LIFF_ID: '2007514355-N7ZqG4BK'
+};
+// ถ้าใช้ ES Module ให้เพิ่ม export:
+// export default CONFIG;
