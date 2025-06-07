3d3444c0ed7821c09fea9c62fdd6e397f8209db7
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-07 20:37:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwzjlrbM1XmUhpUg1nWCij7-0wq1CRhixz-uA9cwv58qMK5Hp1xq03XZDeVnAR6C3WheA/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycby1prRB6Vpn7nfauAWXYNut9ESXgqloJpdua1WaE0DA4i21gV3yaDRaNiGSqxo_-SUNRg/exec',
   LIFF_ID: '2007514355-N7ZqG4BK'
 };
 // ถ้าใช้ ES Module ให้เพิ่ม export:
