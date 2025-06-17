4c7150d003bdd9df3af3949fe3b0a7c9eb384f91
llll12t <147978645+llll12t@users.noreply.github.com>
2025-06-17 15:23:12 +0700

    Update config-admin.js

--- config-admin.js
+++ config-admin.js
@@ -6,4 +6,3 @@ const CONFIG = {
       CLOUD_FUNCTION_GET_URL: 'https://script.google.com/macros/s/AKfycbwwrFy3cy0_zEzRUfddGXwt2mD-pvv8C65UyjkhB7raOVi3cHPVuFOcStoQNecVBfLW/exec', // <-- YOUR GAS URL HERE
       CLOUD_FUNCTION_POST_URL: 'https://script.google.com/macros/s/AKfycbwwrFy3cy0_zEzRUfddGXwt2mD-pvv8C65UyjkhB7raOVi3cHPVuFOcStoQNecVBfLW/exec', // <-- YOUR GAS URL HERE
     };
-};
